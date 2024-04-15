feb5aec3757488d34ec2b4f11a11f58a4c22786c
ST10378552 <ST10378552@vcconnect.edu.za>
2024-04-15 14:38:15 +0200

    Add project files.

---
+++ Step.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeApp
+{
+    class Step
+    {
+        // Property to hold the description of the step
+        public string Description { get; set; }
+    }
+}
